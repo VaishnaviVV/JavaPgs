e83390ae8f25f9f3a50f06e9885f995706d4d031
VaishnaviVV <66016300+VaishnaviVV@users.noreply.github.com>
2021-03-02 12:33:44 +0530

    Update duplicatecharacters.java

--- duplicatecharacters.java
+++ duplicatecharacters.java
@@ -1,10 +1,19 @@
-package j10_02_2021;
-
-public class duplicatecharacters {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.util.Scanner;
+import java.util.Arrays;
+public class Ques1 {
+ public static void main(String[] args) {
+ Scanner s = new Scanner(System.in);
+ StringBuffer a = new StringBuffer();
+ a.append(s.next());
+ int[] ct = new int[256];
+ for (int i = 0; i < a.length(); i++) {
+ int x = a.charAt(i);
+ ct[x]++;
+ }
+ for (int i = 0; i < ct.length; i++) {
+ if (ct[i]!=0) {
+ System.out.println("The character "+(char)i+" appears "+ct[i]+" times in the word");
+ }
+ }
+ }
 }
